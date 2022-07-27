8eee6f3db0bb3631280f8b66052ad7d196d84c14
Hoang Trung <lhtrung307@gmail.com>
2022-07-27 11:04:10 +0700

    More unit test for membership

--- pallets/gafi-membership/src/tests.rs
+++ pallets/gafi-membership/src/tests.rs
@@ -1,12 +1,35 @@
-use crate::{mock::*, Error, Config};
-use frame_support::{assert_err, assert_ok, traits::Currency};
+use crate::{mock::*, Error};
+use codec::Encode;
+use frame_support::{assert_ok, traits::Currency, assert_err};
+use gafi_primitives::{ticket::{TicketLevel, SystemTicket}, system_services::SystemPool, currency::{unit, NativeToken::GAKI}};
+use sp_io::hashing::blake2_256;
 
-// TODO: Add more test later.
 #[test]
 fn membership_registration_should_work() {
 	new_test_ext().execute_with(|| {
-		let _ = pallet_balances::Pallet::<Test>::deposit_creating(&ALICE, 1_000_000);
+		run_to_block(10);
+
+		let _ = pallet_balances::Pallet::<Test>::deposit_creating(&ALICE, 1_000_000 * unit(GAKI));
+
+		let _result = upfront_pool::Pallet::<Test>::join(ALICE, (SystemTicket::Upfront(TicketLevel::Basic)).using_encoded(blake2_256));
+
+		run_to_block(2000);
 
 		assert_ok!(GafiMembership::registration(Origin::signed(ALICE)));
+	});
+}
+
+#[test]
+fn membership_registration_should_get_error_not_eligible() {
+	new_test_ext().execute_with(|| {
+		run_to_block(10);
+
+		let _ = pallet_balances::Pallet::<Test>::deposit_creating(&ALICE, 1_000_000 * unit(GAKI));
+
+		let _result = upfront_pool::Pallet::<Test>::join(ALICE, (SystemTicket::Upfront(TicketLevel::Basic)).using_encoded(blake2_256));
+
+		run_to_block(20);
+
+		assert_err!(GafiMembership::registration(Origin::signed(ALICE)), <Error<Test>>::NotEligibleForRegistration);
 	});
 }
