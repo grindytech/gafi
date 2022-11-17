99c351fae0e1ed7aca3094c38db7aea85f8ed5e7
stefan muto <mutobui@gmail.com>
2022-11-17 17:12:18 +0700

    fix client test

--- runtime/devnet/src/lib.rs
+++ runtime/devnet/src/lib.rs
@@ -283,7 +283,7 @@ impl proof_address_mapping::Config for Runtime {
 
 parameter_types! {
 	pub GameCreatorReward: Permill = Permill::from_percent(30_u32);
-	pub GasPrice: u128 = 4_000_000_000_u128;
+	pub GasPrice: u128 = 4 * microcent(GAKI); // 0.000004 Gaki
 }
 
 impl gafi_tx::Config for Runtime {
