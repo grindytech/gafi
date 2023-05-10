6c8c487355edbfa6264433f5429948628fb9b3f3
stefan muto <mutobui@gmail.com>
2023-05-10 17:34:29 +0700

    create item

--- game/pallet-game/src/features/mod.rs
+++ game/pallet-game/src/features/mod.rs
@@ -1,9 +1,14 @@
-pub mod game_setting;
 pub mod create_collection;
-pub mod id;
-pub mod roles;
-
-pub use game_setting::*;
 pub use create_collection::*;
+
+pub mod game_setting;
+pub use game_setting::*;
+
+pub mod id;
 pub use id::*;
+
+pub mod roles;
 pub use roles::*;
+
+pub mod create_item;
+pub use create_item::*;
