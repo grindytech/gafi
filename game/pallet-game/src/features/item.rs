bec880fa1152e2730252330f102360bf79830c59
stefan muto <mutobui@gmail.com>
2023-05-15 14:41:22 +0700

    batch mint

--- game/pallet-game/src/features/item.rs
+++ game/pallet-game/src/features/item.rs
@@ -9,6 +9,7 @@ impl<T: Config<I>, I: 'static> Mutable<T::AccountId, T::GameId, T::CollectionId,
 		who: T::AccountId,
 		collection_id: T::CollectionId,
 		target: T::AccountId,
+		amount: Amount,
 	) -> DispatchResult {
 		let mut fee = BalanceOf::<T, I>::default();
 
@@ -19,21 +20,51 @@ impl<T: Config<I>, I: 'static> Mutable<T::AccountId, T::GameId, T::CollectionId,
 			collection_owner = owner;
 		}
 		if let Some(config) = GameCollectionConfigOf::<T, I>::get(collection_id) {
-			fee = config.mint_settings.mint_settings.price.unwrap();
+			fee = config.mint_settings.mint_settings.price.unwrap_or_default()
 		}
 		// make a deposit
 		<T as pallet::Config<I>>::Currency::transfer(
 			&who,
 			&collection_owner,
-			fee,
+			fee * amount.into(),
 			ExistenceRequirement::KeepAlive,
 		)?;
 
 		// random mint
-		if let Ok(item) = Self::withdraw_reserve(collection_id) {
-			Self::add_item_balance(target, collection_id, item)?;
+		let mut total_item = 0_u32;
+		{
+			let source = ItemReserve::<T, I>::get(collection_id);
+
+			for item in source.clone() {
+				total_item += item.amount;
+			}
 		}
 
+		ensure!(total_item > 0, Error::<T, I>::SoldOut);
+		ensure!(amount <= total_item, Error::<T, I>::ExceedTotalAmount);
+		if let Some(max_mint) = Self::get_max_mint_amount(collection_id) {
+			ensure!(amount <= max_mint, Error::<T, I>::ExceedAllowedAmount);
+		}
+
+		let mut minted_items: Vec<T::ItemId> = [].to_vec();
+		for _ in 0..amount {
+			let position = Self::random_number(total_item) % total_item;
+			match Self::withdraw_reserve(collection_id, position) {
+				Ok(item) => {
+					Self::add_item_balance(target.clone(), collection_id, item)?;
+					minted_items.push(item);
+				},
+				Err(err) => {
+					return Err(err.into())
+				},
+			};
+		}
+		Self::deposit_event(Event::<T, I>::Minted {
+			minter: who,
+			target,
+			collection_id,
+			minted_items: minted_items,
+		});
 		Ok(())
 	}
 
@@ -73,20 +104,10 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		return random_number
 	}
 
-	pub fn withdraw_reserve(collection_id: T::CollectionId) -> Result<T::ItemId, Error<T, I>> {
-		let mut total_item = 0_u32;
-		{
-			let source = ItemReserve::<T, I>::get(collection_id);
-
-			for item in source.clone() {
-				total_item += item.amount;
-			}
-		}
-
-		ensure!(total_item > 0, Error::<T, I>::SoldOut);
-
-		let position = Self::random_number(total_item);
-
+	pub fn withdraw_reserve(
+		collection_id: T::CollectionId,
+		position: u32,
+	) -> Result<T::ItemId, Error<T, I>> {
 		let result = ItemReserve::<T, I>::try_mutate(collection_id, |reserve_vec| {
 			let mut tmp = 0_u32;
 			for reserve in reserve_vec.into_iter() {
@@ -100,9 +121,9 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 					tmp += reserve.amount;
 				}
 			}
-			Err(Error::<T, I>::ExceedAmount)
+			Err(Error::<T, I>::WithdrawReserveFailed)
 		})
-		.map_err(|_| Error::<T, I>::ExceedAmount);
+		.map_err(|_| Error::<T, I>::WithdrawReserveFailed);
 
 		match result {
 			Ok(item) => Ok(item.item),
@@ -118,5 +139,12 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		let amount = ItemBalances::<T, I>::get((&collection, &who, &item));
 		ItemBalances::<T, I>::insert((collection, who, item), amount + 1);
 		Ok(())
+	}
+
+	pub fn get_max_mint_amount(collection: T::CollectionId) -> Option<u32> {
+		match GameCollectionConfigOf::<T, I>::get(collection) {
+			Some(config) => return config.mint_settings.amount,
+			None => None,
+		}
 	}
 }
