0f09be865dd32510c8b54be755f28bf97ffa8056
stefan muto <mutobui@gmail.com>
2023-05-25 18:47:58 +0700

    not producing blocks

--- node/game3/src/chain_spec.rs
+++ node/game3/src/chain_spec.rs
@@ -1,10 +1,12 @@
+use gafi_support::common::{unit, NativeToken::GAFI};
 use game3_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature, SudoConfig,
 	SystemConfig, WASM_BINARY,
 };
+use hex_literal::hex;
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_core::{sr25519, Pair, Public};
+use sp_core::{crypto::UncheckedInto, sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
@@ -54,10 +56,22 @@ pub fn development_config() -> Result<ChainSpec, String> {
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
 				],
 				true,
 			)
@@ -89,23 +103,62 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 			testnet_genesis(
 				wasm_binary,
 				// Initial PoA authorities
-				vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
+				vec![
+					authority_keys_from_seed("Alice"),
+					authority_keys_from_seed("Bob"),
+				],
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
-					get_account_id_from_seed::<sr25519::Public>("Eve"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Charlie"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Dave"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Eve"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+						1_000_000_u128 * unit(GAFI),
+					),
 				],
 				true,
 			)
@@ -124,12 +177,47 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	))
 }
 
+pub fn gafi_testnet_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"gafi-test",
+		// ID
+		"gafi-test",
+		ChainType::Live,
+		move || {
+			testnet_genesis(
+				wasm_binary,
+				// Initial PoA authorities
+				vec![],
+				// Sudo account
+				hex!("6e312c24b61893b64fbb04fd37b2cc6c1df62a4419f1327fbabad172182a395c").into(),
+				// Pre-funded accounts
+				vec![],
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		None,
+		None,
+		// Properties
+		None,
+		// Extensions
+		None,
+	))
+}
+
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
+	endowed_accounts: Vec<(AccountId, u128)>,
 	_enable_println: bool,
 ) -> GenesisConfig {
 	GenesisConfig {
@@ -139,7 +227,7 @@ fn testnet_genesis(
 		},
 		balances: BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
+			balances: endowed_accounts.iter().cloned().map(|(k, balance)| (k, balance)).collect(),
 		},
 		aura: AuraConfig {
 			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
