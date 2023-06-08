6b85290f3dca604044ebea75c84f044234696394
stefan muto <mutobui@gmail.com>
2023-06-08 16:16:22 +0700

    web3 only

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,22 +1,18 @@
 // #![warn(missing_docs)]
 
 fn main() -> sc_cli::Result<()> {
+	// #[cfg(feature = "with-gari")]
+	// return gafi_cli::command::run();
 
-	#[cfg(feature = "with-gari")]
-	return gafi_cli::command::run();
+	// #[cfg(feature = "with-dev")]
+	// return solochain::command::run();
 
-	#[cfg(feature = "with-dev")]
-	return solochain::command::run();
+	// #[cfg(feature = "manual-seal")]
+	// return solochain::command::run();
 
 	#[cfg(feature = "with-game3")]
 	return game3_node::command::run();
 
-	#[cfg(feature = "manual-seal")]
-	return solochain::command::run();
-
-	// #[cfg(feature = "runtime-benchmarks")]
-	// return gafi_cli::command::run_gari();
-	
 	#[cfg(feature = "runtime-benchmarks")]
 	return game3_node::command::run();
 }
